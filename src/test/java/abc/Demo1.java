8897e12e7517f35c9fea5c2046ca6eb794562d98
aniketrudrawar <aniketrudrawar07@gmail.com>
2022-11-28 17:57:26 +0530

    aniket

---
+++ src/test/java/abc/Demo1.java
@@ -0,0 +1,21 @@
+package abc;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	@Test
+	public void m1() {
+		
+		System.out.println("m1");
+		
+	}
+	
+	@Test
+	public void m2() {
+		
+		System.out.println("m2");
+		
+	}
+
+}
